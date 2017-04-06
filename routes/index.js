a08f7ac0ccf12f9c81c4300752d00e41f3f890fe
chantebryana <chantebryana@gmail.com>
2017-04-05 21:14:13 -0500

    minor edits to database structure file; also first attempts to create index.js for runcible files

---
+++ routes/index.js
@@ -0,0 +1,40 @@
+// https://expressjs.com/en/starter/hello-world.html
+// https://scotch.io/tutorials/use-ejs-to-template-your-node-application
+
+var express = require('express');
+var app = express();
+var router = express.Router();
+
+app.get('/', function(req, res) {
+	res.send('Hello World!');
+});
+
+app.listen(3000, function() {
+	console.log('Example app listening on port 3000!');
+});
+
+router.get('/', function(req, res) {
+	// http://www.w3resource.com/node.js/nodejs-sqlite.php
+	var sqlite3 = require('sqlite3').verbose();
+	var file = 'fam_beta.db';
+	var db = new sqlite3.Database(file);
+	db.all('SELECT * FROM time_temp', function(err, rows) { 
+		res.render('pages', {title: 'Home', rows: rows});
+	});
+	db.close();
+});
+
+router.get('/form', function(req, res) {
+	res.render('pages/form', {title: 'Form'});
+});
+
+router.post('/formpost', function(req, res) {
+	var sqlite3 = require('sqlite3').verbose();
+	var file = 'fam_beta.db';
+	var db = new sqlite3.Database(file);
+	db.all('INSERT INTO time_temp (date, time_taken, temp_f) VALUES(\'' + req.body['time_temp'] + '\')', function(err, rows) {
+		res.redirect('/');
+	});
+	db.close();
+});
+
