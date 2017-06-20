31cdd32ecf81f0dcaf4fcaf4b39f99296aa850ce
chantebryana <chantebryana@gmail.com>
2017-06-19 19:48:00 -0500

    clean up index.js to remove commented out lines that were cluttering up the space (most were about opening and closing the db for each router.get or router.post)

--- routes/index.js
+++ routes/index.js
@@ -20,31 +20,19 @@ var sqlite3 = require('sqlite3').verbose();
 var file = 'fam_beta.db';
 var db = new sqlite3.Database(file);
 
-/*
-router.get('/', function(req, res) {
-	res.render('pages/index.ejs', {title: 'Home'});
-});
-*/
 router.get('/form', function(req, res) {
 	res.render('pages/form.ejs', {title: 'Form'});
 });
 
 router.get('/form_update', function(req, res) {
-/*
-	var sqlite3 = require('sqlite3').verbose();
-	var file = 'fam_beta.db';
-	var db = new sqlite3.Database(file);
-	var query = "";
-*/
 	db.all(query='SELECT * FROM time_temp WHERE id = ' + req.query.id, function(err, rows_from_db) { 
 		console.log("attempted to query database with __" + query + "__");
 		res.render('pages/form_update.ejs', {title: 'Form Update', row_to_renderer: rows_from_db[0]});
 	});
-	//db.close();
 	//res.render('pages/form_update.ejs', {title: 'Form Update', rows: rows});
 });
 
-var find_cycle_id = function(current_id, previous, callback) {
+var find_cycle_id = function(current_id, offset, callback) {
 	db.all('SELECT name AS id FROM cycles ORDER BY begin_date DESC LIMIT 1', function(err, rows_from_db) { 
 //		console.log("rows from db " + rows_from_db[0].id);
 		callback(rows_from_db[0].id);
@@ -53,11 +41,6 @@ var find_cycle_id = function(current_id, previous, callback) {
 
 router.get('/', function(req, res) {
 	// http://www.w3resource.com/node.js/nodejs-sqlite.php
-/*
-	var sqlite3 = require('sqlite3').verbose();
-	var file = 'fam_beta.db';
-	var db = new sqlite3.Database(file);
-*/
 	find_cycle_id(null, null, function(cycle_id) {
 		//console.log("rows from db " + cycle_id);
 		db.all('SELECT * FROM time_temp WHERE cycle = "' + cycle_id + '" ORDER BY date', function(err, rows_from_db) { 
@@ -75,50 +58,29 @@ router.get('/', function(req, res) {
 		});
 	};
 */
-	//db.close();
 });
 
 router.post('/formpost', function(req, res) {
-/*
-	var sqlite3 = require('sqlite3').verbose();
-	var file = 'fam_beta.db';
-	var db = new sqlite3.Database(file);
-*/
 	db.all("INSERT INTO time_temp (date, time_taken, temp_f) VALUES( \" " + req.body["date"] + " \", \" " + req.body["time_taken"] + " \", \" " + req.body["temp_f"] + " \")", function(err, rows) {
 		res.redirect('/');
 	});
-	//db.close();
 });
 
 
 router.post('/deletepost', function(req, res) {
-/*
-	var sqlite3 = require('sqlite3').verbose();
-	var file = 'fam_beta.db';
-	var db = new sqlite3.Database(file);
-*/
 	var query = "";
 	db.all(query="DELETE FROM time_temp WHERE id=" + req.body["id"], function(err, rows) {
 		console.log("attempted to delete with ((" + query + "))");
 		res.redirect('/');
 	});
-
-	//db.close();
 });
 
 router.post('/form_post_update', function(req, res) {
-/*
-	var sqlite3 = require('sqlite3').verbose();
-	var file = 'fam_beta.db';
-	var db = new sqlite3.Database(file);
-*/
 	var query = "";
 	db.all(query="UPDATE time_temp SET date=\"" + req.body["date"] + "\", time_taken=\"" + req.body["time_taken"] + "\", temp_f=" + req.body["temp_f"] + " WHERE id=" + req.body["id"], function(err, rows) {
 		console.log("attempted to update with ((" + query + "))");
 		res.redirect('/');
 	});
-
-	//db.close();
 });
 
 app.listen(3000, function() {
