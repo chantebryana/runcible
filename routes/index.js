3e33967f689b267a1cf2751ff8f5f2c30880b1de
chantebryana <chantebryana@gmail.com>
2017-06-22 15:10:27 -0500

    made the query in "else" dynamic: if there's no req.query.cycle in the url, the page defaults to the most recent cycle, even if the db's been updated

--- routes/index.js
+++ routes/index.js
@@ -31,7 +31,7 @@ router.get('/form_update', function(req, res) {
 	});
 	//res.render('pages/form_update.ejs', {title: 'Form Update', rows: rows});
 });
-
+/*
 var find_cycle_id = function(current_id, offset, callback) {
 	db.all('SELECT id FROM cycles WHERE id =' + offset + ' ORDER BY begin_date DESC', function(err, rows_from_db) { 
 //		console.log("rows from db " + rows_from_db[0].id);
@@ -39,10 +39,13 @@ var find_cycle_id = function(current_id, offset, callback) {
 		callback(rows_from_db[0].id);
 	});
 };
+*/
 
+// this function looks up the beginning and ending "brackets" of paging through each cycle: laying out all of the id's in cycle table, storing the data in a simple array, and then using the indeces of the array to look up the id of the current cycle, as well as the next and previous ones.  This is all used in res.render('/'...) to help with printing the correct cycle on the home page of my site:
 function cycle_brackets(current_cycle_id, callback){
+	// this db query looks up the ids of all entries in cycles table, ordered by date (not id) in descending order: 
 	db.all('SELECT id FROM cycles ORDER BY begin_date DESC', function(err, rows_from_db) { 
-		// empty array to hold the id's (in name-order) of "cycles" table:
+		// empty array to hold the id's (in descending-date-order) of "cycles" table:
 		var cycle_id_array = []; 
 		// finds the length of the array-object returned by the sql query, and pops that into the for loop length:
 		var cycles_length = Object.keys(rows_from_db).length; 
@@ -52,18 +55,18 @@ function cycle_brackets(current_cycle_id, callback){
 		};
 		console.log(cycle_id_array);
 
-		// var current_cycle_id_index equals array index of current_cycle_id (or if there's no current_cycle_id, then it's -1 which is outside the scope of the array), then use current_cycle_id_index to math out previous_cycle_id and next_cycle_id(the math is backwards because the array is in descending order):
-		var current_cycle_id_index = 0; // CE: fix this 
+		// var current_cycle_id_index equals array index of current_cycle_id (or if there's no current_cycle_id, then it defaults to 0, or the most recent cycle at the beginning of the array), then use current_cycle_id_index to math out previous_cycle_id and next_cycle_id (the math is backwards because the array is in descending order):
+		var current_cycle_id_index = 0; 
 		for (i = 0; i < cycles_length; i++) {
 			if (cycle_id_array[i] == current_cycle_id) { 
 				current_cycle_id_index = i;
 			};
 		};
 		console.log("current_cycle_id_index: " + current_cycle_id_index);
-		var previous_cycle_id = cycle_id_array[current_cycle_id_index+1];
-		var next_cycle_id = cycle_id_array[current_cycle_id_index-1];
+		var previous_cycle_id = cycle_id_array[current_cycle_id_index + 1];
+		var next_cycle_id = cycle_id_array[current_cycle_id_index - 1];
 		console.log("previous: " + previous_cycle_id + ", next: " + next_cycle_id);
-		callback(previous_cycle_id, next_cycle_id);
+		callback(previous_cycle_id, next_cycle_id, cycle_id_array);
 	});
 };
 
@@ -79,7 +82,7 @@ router.get('/', function(req, res) {
 	};
 	console.log("cycle from index.ejs hyperlink: " + cycle_offset);
 	*/
-	cycle_brackets(current_cycle, function(previous_cycle, next_cycle) {
+	cycle_brackets(current_cycle, function(previous_cycle, next_cycle, cycle_id_array) {
 		if (req.query.cycle) {		
 			db.all('SELECT * FROM time_temp WHERE cycle_id = "' + current_cycle + '" ORDER BY date', function(err, rows_from_db) { 
 				res.render('pages', {
@@ -92,8 +95,8 @@ router.get('/', function(req, res) {
 					}
 				});
 			});
-		} else {
-			db.all('SELECT * FROM time_temp WHERE cycle_id = "2" ORDER BY date', function(err, rows_from_db) { 
+		} else { // CE: fix "2" below to something dynamic:
+			db.all('SELECT * FROM time_temp WHERE cycle_id = "'+ cycle_id_array[0] +'" ORDER BY date', function(err, rows_from_db) { 
 				res.render('pages', {
 					title: 'Home', 
 					rows_to_renderer: rows_from_db, 
