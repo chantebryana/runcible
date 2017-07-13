704b04cb357b16b049b1f1976fec7426f5d9e7c7
chantebryana <chantebryana@gmail.com>
2017-07-13 09:27:16 -0500

    updated router.post('/form_post_update'...) on index.js: db query includes update to cycle_id, which is the missing piece I knew I needed but got confused for a moment during my last commit

--- routes/index.js
+++ routes/index.js
@@ -148,7 +148,7 @@ router.post('/deletepost', function(req, res) {
 
 router.post('/form_post_update', function(req, res) {
 	var query = "";
-	db.all(query="UPDATE time_temp SET date=\"" + req.body["date"] + "\", time_taken=\"" + req.body["time_taken"] + "\", temp_f=" + req.body["temp_f"] + " WHERE id=" + req.body["id"], function(err, rows) {
+	db.all(query="UPDATE time_temp SET date=\"" + req.body["date"] + "\", time_taken=\"" + req.body["time_taken"] + "\", temp_f=" + req.body["temp_f"] + ", cycle_id=" + req.body["cycle_id"] + " WHERE id=" + req.body["id"], function(err, rows) {
 		console.log("attempted to update with ((" + query + "))");
 		res.redirect('/');
 	});
