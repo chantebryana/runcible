2acc9c195673e9ebab501bbb0004a94b93ab67c5
chantebryana <chantebryana@gmail.com>
2017-07-18 14:22:00 -0500

    accessed hash key via calling map_array_hash, thanks to some help and learning from Jim

--- zzSandbox/20170718 CE plays with Jimbo Async Lesson.js
+++ zzSandbox/20170718 CE plays with Jimbo Async Lesson.js
@@ -1,20 +1,21 @@
 var name_age_arr = [{name: 'Tony', age: 33}, {name: 'Tanya', age: 27}, {name: 'Fatima', age: 41}];
 
-function map_array_hash(array_hash, callback) {
-	var name = [];
-	for(i = 0; i < array_hash.length; i++) {
-		//console.log(array_hash[i].name);
-		name[i] = array_hash[i].name;
-		//console.log(name[i]);
-	}
-	//console.log(name);
-	callback(name);
+function print(array) {
+	for(i = 0; i < array.length; i++) {
+		console.log(array[i]);
+	};
 };
 
-map_array_hash(name_age_arr, function(name) {
-	for(i = 0; i < name.length; i++) {
-		console.log(name[i]);
-	};
+function map_array_hash(array_hash, key, callback) {
+	var array = [];
+	for(i = 0; i < array_hash.length; i++) {
+		array[i] = array_hash[i][key]; // not .key or not ['key']
+	}
+	callback(array);
+};
+
+map_array_hash(name_age_arr, 'age', function(array) {
+	print(array);
 });
 //Tony
 //Tanya
