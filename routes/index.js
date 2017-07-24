733e837ac8d7bb6570fe0c6cd52ea1bea87061c5
chantebryana <chantebryana@gmail.com>
2017-07-24 10:10:27 -0500

    successfully re-create the callback functions that feed into router.get('/'...), and also successfully render a proper web page.  Feels pretty good.

--- routes/index.js
+++ routes/index.js
@@ -120,7 +120,7 @@ router.get('/', function(req, res) {
 	});
 });
 */
-
+///*
 function get_cycle_id_by_date(callback) {
 	db.all('SELECT id FROM cycles ORDER BY begin_date DESC', function(err, rows_from_db) {
 		var cycle_id_array = [];
@@ -141,12 +141,54 @@ function get_first_and_last_cycle_id(callback) {
 	});
 };
 
+function find_next_previous_cycle(current_cycle_id, callback) {
+	get_cycle_id_by_date(function (cycle_id_array){
+		var current_cycle_id_index = 0;
+		for (i = 0; i < cycle_id_array.length; i++) {
+			if (cycle_id_array[i] == current_cycle_id) {
+				current_cycle_id_index = i;
+			};
+		};
+		var previous_cycle_id = cycle_id_array[current_cycle_id_index + 1];
+		var next_cycle_id = cycle_id_array[current_cycle_id_index - 1];
+		callback(previous_cycle_id, next_cycle_id);
+	});
+};
+
 router.get('/', function(req,res) {
+	var current_cycle_id = req.query.cycle;
 	get_first_and_last_cycle_id(function(first_cycle_id, last_cycle_id){
-		console.log('first_cycle_id: ' + first_cycle_id + ', last_cycle_id: ' + last_cycle_id + '\n');
+		find_next_previous_cycle(current_cycle_id, function(previous_cycle_id, next_cycle_id){
+			var which_cycle_id = last_cycle_id;
+			if (req.query.cycle) {
+				which_cycle_id = current_cycle_id;
+			};
+			db.all('SELECT *, strftime(\'%m/%d\', date) as \'month_day\' FROM time_temp WHERE cycle_id = "' + which_cycle_id + '" ORDER BY date', function(err, rows_from_db) { 
+				var temp_array = []
+				var date_array = []
+				for (var i = 0; i < rows_from_db.length; i++){
+					temp_array[i] = rows_from_db[i].temp_f
+					date_array[i] = "\"" + rows_from_db[i].month_day + "\""
+				}
+				// res.render sends various variables to index.ejs and its dependent pages:
+				res.render('pages', {
+					title: 'Home', 
+					rows_to_renderer: rows_from_db, 
+					temp_array_to_renderer: temp_array,
+					date_array_to_renderer: date_array,
+					cycle_id_to_renderer: {
+						prev: previous_cycle_id, 
+						curr: current_cycle_id, 
+						next: next_cycle_id, 
+						first: first_cycle_id, 
+						last: last_cycle_id
+					}
+				});
+			});
+		});
 	});
 });
-
+//*/
 
 router.post('/formpost', function(req, res) {
 	db.all("INSERT INTO time_temp (date, time_taken, temp_f, cycle_id) VALUES( \" " + req.body["date"] + " \", \" " + req.body["time_taken"] + " \", \" " + req.body["temp_f"] + " \", \" " + req.body["cycle_id"] + " \")", function(err, rows) {
--- zzSandbox/20170717 cycle_array_order_by_id func.js
+++ zzSandbox/20170717 cycle_array_order_by_id func.js
@@ -19,12 +19,63 @@ function get_first_and_last_cycle_id(callback) {
 	});
 };
 
+function find_next_previous_cycle(current_cycle_id, callback) {
+	get_cycle_id_by_date(function (cycle_id_array){
+		var current_cycle_id_index = 0;
+		for (i = 0; i < cycle_id_array.length; i++) {
+			if (cycle_id_array[i] == current_cycle_id) {
+				current_cycle_id_index = i;
+			};
+		};
+		var previous_cycle_id = cycle_id_array[current_cycle_id_index + 1];
+		var next_cycle_id = cycle_id_array[current_cycle_id_index - 1];
+		callback(previous_cycle_id, next_cycle_id);
+	});
+};
+
+router.get('/', function(req,res) {
+	var current_cycle_id = req.query.cycle;
+	get_first_and_last_cycle_id(function(first_cycle_id, last_cycle_id){
+		find_next_previous_cycle(current_cycle_id, function(previous_cycle_id, next_cycle_id){
+			var which_cycle_id = last_cycle_id;
+			if (req.query.cycle) {
+				which_cycle_id = current_cycle_id;
+			};
+			db.all('SELECT *, strftime(\'%m/%d\', date) as \'month_day\' FROM time_temp WHERE cycle_id = "' + which_cycle_id + '" ORDER BY date', function(err, rows_from_db) { 
+				var temp_array = []
+				var date_array = []
+				for (var i = 0; i < rows_from_db.length; i++){
+					temp_array[i] = rows_from_db[i].temp_f
+					date_array[i] = "\"" + rows_from_db[i].month_day + "\""
+				}
+				// res.render sends various variables to index.ejs and its dependent pages:
+				res.render('pages', {
+					title: 'Home', 
+					rows_to_renderer: rows_from_db, 
+					temp_array_to_renderer: temp_array,
+					date_array_to_renderer: date_array,
+					cycle_id_to_renderer: {
+						prev: previous_cycle_id, 
+						curr: current_cycle_id, 
+						next: next_cycle_id, 
+						first: first_cycle_id, 
+						last: last_cycle_id
+					}
+				});
+			});
+		});
+	});
+});
+/*
 router.get('/', function(req,res) {
 	get_first_and_last_cycle_id(function(first_cycle_id, last_cycle_id){
 		console.log('first_cycle_id: ' + first_cycle_id + ', last_cycle_id: ' + last_cycle_id + '\n');
+//	});
+		find_next_previous_cycle(5, function(previous_cycle_id, next_cycle_id){
+			console.log('previous_cycle_id: ' + previous_cycle_id + ', next_cycle_id: ' + next_cycle_id + '\n');
+		});
 	});
 });
-
 
 router.get('/', function(req,res) {
 	get_cycle_id_by_date(function (cycle_id_array){
@@ -33,12 +84,6 @@ router.get('/', function(req,res) {
 		console.log('first_cycle_id: ' + first_cycle_id + ', last_cycle_id: ' + last_cycle_id + '\n');
 	});
 });
-
-function find_next_previous_cycle(current_cycle_id, callback) {
-	get_cycle_id_by_date(function (cycle_id_array){
-		
-	});
-};
 
 
 function cycle_brackets(cycle_id_array, current_cycle_id, callback) {
@@ -66,3 +111,5 @@ router.get('/'...) {
 	};
 	...
 };
+*/
+
