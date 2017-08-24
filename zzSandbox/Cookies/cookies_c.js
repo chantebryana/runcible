2f105b16acbdf09d0e36fd371dc58e84c70641a9
chantebryana <chantebryana@gmail.com>
2017-08-24 18:05:53 -0500

    more pseudocode changes

--- zzSandbox/Cookies/cookies_c.js
+++ zzSandbox/Cookies/cookies_c.js
@@ -12,6 +12,26 @@ CREATE TABLE cookies (
 session_token = "1000"; // this can and should be much more complicated
 session_data = "42"; // this integer-string represents the number of page loads
 
+
+
+// playing with server-side code that generates a new session_token (super-super simplified):
+function new_session_token(callback) {
+	// query session_token from db table, sorting in descending order so that largest session_token is on the top of the list (rows array):
+	db.all("SELECT session_token FROM cookies ORDER BY session_token DESC",function(err, rows){
+		// iterate the first element of rows by 1 so that the new session token is one larger than the previous one:
+		rows[0] += 1; // (CE confirmed that this works on normal arrays)
+		// pass iterated rows variable through callback to be used later on in server-side code:
+		callback(rows);
+	});
+};
+// what new_session_token looks like when called:
+new_session_token(function(/*  */) {
+
+});
+
+
+
+
 // playing around with some flow control:
 
 // cookie syntax example (i think): 
