97128bdad27fe6d1304813ec8e4bfbd5502c6730
chantebryana <chantebryana@gmail.com>
2017-08-25 22:40:39 -0500

    some proto notes

--- zzSandbox/chartist/chartist_server_side_xy.js
+++ zzSandbox/chartist/chartist_server_side_xy.js
@@ -1,4 +1,52 @@
 // try to populate {x: ,y: } array-object (with y gaps) manually 
 
 // what var rows might look like after a db table query:
-var rows = 
+var rows = [
+  { id: 192,
+    date: '2012-10-15',
+    time_taken: '07:30',
+    temp_f: 97.7,
+    cycle_id: 7,
+    notes: '',
+    entry_created: '2017-08-25 16:20:00',
+    month_day: '10/15' },
+  { id: 195,
+    date: '2012-10-18',
+    time_taken: '07:30',
+    temp_f: 98.2,
+    cycle_id: 7,
+    notes: '',
+    entry_created: '2017-08-25 16:20:00',
+    month_day: '10/18' },
+  { id: 196,
+    date: '2012-10-19',
+    time_taken: '09:00',
+    temp_f: 98.3,
+    cycle_id: 7,
+    notes: '',
+    entry_created: '2017-08-25 16:20:00',
+    month_day: '10/19' },
+  { id: 197,
+    date: '2012-10-20',
+    time_taken: '07:30',
+    temp_f: 98.3,
+    cycle_id: 7,
+    notes: '',
+    entry_created: '2017-08-25 16:20:00',
+    month_day: '10/20' },
+  { id: 198,
+    date: '2012-10-21',
+    time_taken: '11:30',
+    temp_f: 98.2,
+    cycle_id: 7,
+    notes: '',
+    entry_created: '2017-08-25 16:20:00',
+    month_day: '10/21' }
+];
+
+// what i need to match up on chartist side: 
+labels: ['', '', '', ''] // x axis text
+series: [ , , , ] // y axis integers or undefined
+// also consider syncing up with cycles table to determine what the beginning and end date should be (instead of relying on gap-prone time_temp table info)
+
+
