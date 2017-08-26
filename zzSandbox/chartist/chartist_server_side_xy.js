66163b520278116b4c85817d829edc972acd2333
chantebryana <chantebryana@gmail.com>
2017-08-26 00:07:51 -0500

    super pseudocode to try to manually populate x and y axis labels / coordinates that includes gaps in the date (lots to wrap my head around, still in very proto phase)

--- zzSandbox/chartist/chartist_server_side_xy.js
+++ zzSandbox/chartist/chartist_server_side_xy.js
@@ -49,4 +49,30 @@ labels: ['', '', '', ''] // x axis text
 series: [ , , , ] // y axis integers or undefined
 // also consider syncing up with cycles table to determine what the beginning and end date should be (instead of relying on gap-prone time_temp table info)
 
+// use these variables: 
+// taken from cycles table: begin date and end date of current cycle: 
+begin_date // eg 2012-09-23
+end_date // eg 2012-10-22 or 'Today'
 
+// find a way to populate with iterative dates based on begin and end date (generated form cycles table): 
+var date_range = [/*series of dates!*/];
+
+for (var i = 0; i < rows.length; i++) { // i don't know which 'rows' i'll be using here
+	if(end_date != 'Today') {
+		// use end_date from cycles as the logic point
+		labels[i] = date_range[i]
+	} else {
+		// use end_date_int from time_temp table as logic point
+		labels[i] = rows_from_db[i].date // i'm pretty sure this is right
+	}
+}
+
+for (var i = 0; i < rows.length; i++) { // i don't know which 'rows' i'll be using here
+	if (/*rows[i].date == labels[i]*/) { // if there's a date that matches label array
+		series[i] = rows[i].temp_f;
+	} else { // if there's no entry for this date
+		series[i] = undefined;
+	}
+
+
+}
