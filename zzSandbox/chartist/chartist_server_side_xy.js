097cdc57dc710ce4fb3a86e15070bfc0d7e7329a
chantebryana <chantebryana@gmail.com>
2017-08-28 20:08:43 -0500

    better datetime format from sqlite table query: let's try to integrate this to make my function brains have less of a headache ;-)

--- zzSandbox/chartist/chartist_server_side_xy.js
+++ zzSandbox/chartist/chartist_server_side_xy.js
@@ -1,6 +1,13 @@
 // try to populate {x: ,y: } array-object (with y gaps) manually 
 
+SELECT *, strftime() 
+
+
+
 // what var rows might look like after a db table query:
+/* datetime created from this query: 
+SELECT *, strftime('%Y-%m-%d %H:%M:%S', date) as 'datetime' FROM time_temp WHERE cycle_id = 7 ORDER BY id;
+*/
 var rows = [
   { id: 192,
     date: '2012-10-15',
@@ -9,7 +16,8 @@ var rows = [
     cycle_id: 7,
     notes: '',
     entry_created: '2017-08-25 16:20:00',
-    month_day: '10/15' },
+    //month_day: '10/15' },
+		datetime: '2012-10-15 00:00:00' },
   { id: 195,
     date: '2012-10-18',
     time_taken: '07:30',
@@ -17,7 +25,8 @@ var rows = [
     cycle_id: 7,
     notes: '',
     entry_created: '2017-08-25 16:20:00',
-    month_day: '10/18' },
+    //month_day: '10/18' },
+		datetime: '2012-10-18 00:00:00' },
   { id: 196,
     date: '2012-10-19',
     time_taken: '09:00',
@@ -25,7 +34,8 @@ var rows = [
     cycle_id: 7,
     notes: '',
     entry_created: '2017-08-25 16:20:00',
-    month_day: '10/19' },
+    //month_day: '10/19' },
+		datetime: '2012-10-19 00:00:00' },
   { id: 197,
     date: '2012-10-20',
     time_taken: '07:30',
@@ -33,7 +43,8 @@ var rows = [
     cycle_id: 7,
     notes: '',
     entry_created: '2017-08-25 16:20:00',
-    month_day: '10/20' },
+    //month_day: '10/20' },
+		datetime: '2012-10-20 00:00:00' },
   { id: 198,
     date: '2012-10-21',
     time_taken: '11:30',
@@ -41,7 +52,8 @@ var rows = [
     cycle_id: 7,
     notes: '',
     entry_created: '2017-08-25 16:20:00',
-    month_day: '10/21' }
+    //month_day: '10/21' }
+		datetime: '2012-10-21 00:00:00' }
 ];
 
 // what i need to match up on chartist side: 
