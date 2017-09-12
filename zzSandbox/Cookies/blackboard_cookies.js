d57ef0837eb0434325737a2b1549514205ebe0ca
chantebryana <chantebryana@gmail.com>
2017-09-11 21:42:01 -0500

    play space for cookies and related

---
+++ zzSandbox/Cookies/blackboard_cookies.js
@@ -0,0 +1,10 @@
+var dict1 = {'key1':{'vkey1': 'vvalue1', 'vkey2': 'vvalue2', 'vkey3': 'vvalue3'}};
+
+var dict2 = {'key1': {'vkey1':'vvalue1', 'vkey2':'vvalue2', 'vkey3':'vvalue3'}, 'key2':'value2'};
+
+hp:firefox				->	session key: 'UUID2300'
+hp:chrome					-> 	session key: 'UUID2400'
+moto:firefos			->	session key: 'UUID2500'
+library:explorer	->	session key: 'UUID2600'
+
+
