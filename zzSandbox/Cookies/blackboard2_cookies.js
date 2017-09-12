4467b676e36688ca1ccc3b79465543e236c1b066
chantebryana <chantebryana@gmail.com>
2017-09-12 13:11:34 -0500

    playing around (slowly) with page_loads and cookies and db queries and rendering to web page and all sorts of fun stuff

---
+++ zzSandbox/Cookies/blackboard2_cookies.js
@@ -0,0 +1,40 @@
+// https://expressjs.com/en/starter/hello-world.html
+// https://scotch.io/tutorials/use-ejs-to-template-your-node-application
+
+var express = require('express');
+var path = require('path'); // makes app.use(express.static ... work down below
+
+var cookieParser = require('cookie-parser');
+var cookie = require('cookie');  // https://www.npmjs.com/package/cookie
+var cookie_var = ""; // CE: temporary cookie variable
+var bodyParser = require('body-parser');
+
+var app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, '../public'))); // accesses public directory
+
+var router = app;//express.Router();
+app.set('view engine', 'ejs');  // line 16 of app.js in Lionheart
+
+// global variables to open and access database
+var sqlite3 = require('sqlite3').verbose();
+var file = 'fam_beta.db';
+var db = new sqlite3.Database(file);
+
+
+
+router.get('/', function(req, res){
+	var browser_secret_cookie = "bbb222";
+	db.all("SELECT page_loads FROM cookie_key_test WHERE session_id = \"" + browser_secret_cookie + "\"", function(err, rows_from_select) {
+		console.log(rows_from_select);
+	});
+});
+
+
+app.listen(3000, function() {
+	console.log('Example app listening on port 3000!');
+});
+
--- zzSandbox/Cookies/blackboard_cookies.js
+++ zzSandbox/Cookies/blackboard_cookies.js
@@ -7,4 +7,43 @@ hp:chrome					-> 	session key: 'UUID2400'
 moto:firefos			->	session key: 'UUID2500'
 library:explorer	->	session key: 'UUID2600'
 
+//	//	//	//
+/*
+COOKIE_TABLE
+	ID | COOKIE_KEY | SESSION_DATA
+	-------------------------------------------------------
+	00 | 'abc123'		| '{"page_loads":00, "key":"value"}'
+*/
 
+// pseudocode for parsing and manipulating session data (page loads), assuming that the cookie key received from the browser is legit (as per discussion w/ Jim 2017-09-11):
+db.all("SELECT session_data FROM cookie_table WHERE cookie_key =\"" + browser_secret_cookie + "\"", function(err, rows){ // rows variable should be a long string
+	// parsing long JSON string and saving to pg_load variable:
+	var pg_load = JSON.parse(rows[0]);
+	// increment pg_load by 1, since presumably I've loaded or refreshed a page to get to this portion of code:
+	pg_load += 1;
+	
+});
+
+
+// how would I do this w/o JSON, but rather with more structured db rows?
+/*
+COOKIE_TABLE
+	ID | SESSION_ID | PAGE_LOADS
+	-------------------------------
+	00 | 'abc123'		| 00
+*/
+
+db.all("SELECT page_loads FROM cookie_table WHERE session_id =\"" + browser_secret_cookie + "\"", function(err, rows) {
+	// rows == [{page_loads:00}] 
+	var pg_load = rows[0].page_loads; // should be 00: raw intiger
+	pg_load += 1;
+	db.all("UPDATE cookie_table SET page_loads=" + pg_load + "WHERE session_id=\"" + browser_secret_cookie + "\"", function(err, rows_from_update) {
+		res.render( {
+			pg_load_to_renderer: pg_load
+		});
+	});
+});
+
+// --	--	---
+
+// I feel like I should test this idea out. I suppose I could create a dummy db table, load it with a couple rows of dummy data, then access and manipulate it using my precursor to real workflow pseudocode, and attempt to iron out the kinks. Sounds good to me.
