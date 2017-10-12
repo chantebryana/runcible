16f56bb0ba5e53d976812e24301cb3a8feb751c4
chantebryana <chantebryana@gmail.com>
2017-10-12 17:31:52 -0500

    clean up (efforts related to relative file paths for fs)

--- zzSandbox/simplify_indexjs/mymoduleserver.js
+++ zzSandbox/simplify_indexjs/mymoduleserver.js
@@ -4,7 +4,7 @@ var http = require('http');
 var dt = require('./myfirstmodule');
 var fs = require('fs');
 
-/*
+
 http.createServer(function(req,res) {
 	fs.readFile('demofile1.html', function(err, data) {
 		res.writeHead(200, {'Content-Type': 'text/html'});
@@ -13,21 +13,4 @@ http.createServer(function(req,res) {
 		res.end();
 	});
 }).listen(8080);
-*/
-
-// https://stackoverflow.com/questions/10058814/get-data-from-fs-readfile
-function readContent(callback) {
-	fs.readFile('./demofile1.html', function(err, content) {
-		if (err) return callback(err)
-		callback(null, content);
-	})
-}
-
-readContent(function (err, content) {
-	console.log(content);
-})
-
-// printout on terminal: 
-// ruby@rubyVM:~/Projects/runcible$ node ./zzSandbox/simplify_indexjs/mymoduleserver.js 
-// undefined
 
