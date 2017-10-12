42b041981bf3521eee7e799c49472bc87f6de5d2
chantebryana <chantebryana@gmail.com>
2017-10-12 12:57:13 -0500

    get asynchronous fs to work too

--- zzSandbox/simplify_indexjs/main.js
+++ zzSandbox/simplify_indexjs/main.js
@@ -1,15 +1,15 @@
 // https://www.tutorialspoint.com/nodejs/nodejs_file_system.htm
 
 var fs = require("fs");
-/*
+
 // Asynchronous read
-fs.readFile('input.txt', function (err, data) {
+fs.readFile('./zzSandbox/simplify_indexjs/input.txt', function (err, data) {
   if (err) {
     return console.error('<<<>>>', err);
   }
   console.log("Asynchronous read: " + data.toString());
 });
-*/
+
 // Synchronous read
 var data = fs.readFileSync('./zzSandbox/simplify_indexjs/input.txt');
 console.log("Synchronous read: " + data.toString());
