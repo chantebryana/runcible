3ee416cfaa4dd1e53a0d4f5692a400c73b7640de
chantebryana <chantebryana@gmail.com>
2017-10-12 18:51:54 -0500

    commiting notes and changes

--- zzSandbox/simplify_indexjs/main.js
+++ zzSandbox/simplify_indexjs/main.js
@@ -8,35 +8,19 @@ fs.readFile('./input.txt', function (err, data) {
     return console.error('<<<>>>', err);
   }
   console.log("Asynchronous read: " + data.toString());
+	// eval(data.toString()); // something like that instead of console.log...
+	// how to eval javascript file into local context? google (versus child or parent context)
 });
 
 // Synchronous read
+//jim says use this
 var data = fs.readFileSync('./input.txt');
 console.log("Synchronous read: " + data.toString());
+//eval line here
 
 console.log('Program ended');
 
 
-
-// relative path notes w/ Jim:
-
-// example from app.js in lionheart: (line 25)
-app.use('/favicon.ico', express.static(path.join(__dirname, 'public', 'favicon.ico')));
-
-//1:
-app.use // for this URL '/favicon.ico' ...
-//2:
-express.static // ... use this file named here ot satisfy a URL request '/favicon.ico'
-//3:
-path.join // knows how to concatenate strings wihtin (): 
-//4:
-__dirname // gives the absolute path (__dirname) to the present working directory
-
-
-//pwd (present working directory; current working directory): 
-//1: ***at in node or invoke sciprt***
-//2: where the script is at
-//3: root (not a relative path, but an absolute path starting at root directory)
 
 
 
