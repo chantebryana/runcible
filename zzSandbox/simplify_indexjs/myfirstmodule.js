ae4afc088eac430f5310bed23225d0d0d9ae93a5
chantebryana <chantebryana@gmail.com>
2017-10-12 10:53:32 -0500

    research re simplifying index.js

---
+++ zzSandbox/simplify_indexjs/myfirstmodule.js
@@ -0,0 +1,5 @@
+// https://www.w3schools.com/nodejs/nodejs_modules.asp
+// using 'exports' keyword to make properties and methods available outside module file:
+exports.myDateTime = function() {
+	return Date();
+}
