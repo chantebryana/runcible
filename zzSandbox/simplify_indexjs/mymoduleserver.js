18bcbe02795094919f13298ff229708491113c13
chantebryana <chantebryana@gmail.com>
2017-10-12 11:12:49 -0500

    research re simplify index.js: try to access file system a couple ways: no dice

--- zzSandbox/simplify_indexjs/mymoduleserver.js
+++ zzSandbox/simplify_indexjs/mymoduleserver.js
@@ -1,9 +1,33 @@
 // // https://www.w3schools.com/nodejs/nodejs_modules.asp
+// https://www.w3schools.com/nodejs/nodejs_filesystem.asp
 var http = require('http');
 var dt = require('./myfirstmodule');
+var fs = require('fs');
 
+/*
 http.createServer(function(req,res) {
-	res.writeHead(200, {'Content-Type': 'text/html'});
-	res.write('The date and time are currently: ' + dt.myDateTime());
-	res.end();
+	fs.readFile('demofile1.html', function(err, data) {
+		res.writeHead(200, {'Content-Type': 'text/html'});
+		res.write(data);
+		//res.write('The date and time are currently: ' + dt.myDateTime());
+		res.end();
+	});
 }).listen(8080);
+*/
+
+// https://stackoverflow.com/questions/10058814/get-data-from-fs-readfile
+function readContent(callback) {
+	fs.readFile('./demofile1.html', function(err, content) {
+		if (err) return callback(err)
+		callback(null, content);
+	})
+}
+
+readContent(function (err, content) {
+	console.log(content);
+})
+
+// printout on terminal: 
+// ruby@rubyVM:~/Projects/runcible$ node ./zzSandbox/simplify_indexjs/mymoduleserver.js 
+// undefined
+
