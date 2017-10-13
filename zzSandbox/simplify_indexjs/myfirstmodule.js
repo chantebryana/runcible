2b067b8efdc60868564679452d58f62862488a69
chantebryana <chantebryana@gmail.com>
2017-10-13 16:39:02 -0500

    playing with fs and eval: sloppy code that works (see comments)

--- zzSandbox/simplify_indexjs/myfirstmodule.js
+++ zzSandbox/simplify_indexjs/myfirstmodule.js
@@ -1,5 +1,12 @@
 // https://www.w3schools.com/nodejs/nodejs_modules.asp
 // using 'exports' keyword to make properties and methods available outside module file:
+
 exports.myDateTime = function() {
 	return Date();
 }
+
+/*
+myDateTime = function() {
+	return Date();
+}
+*/
--- zzSandbox/simplify_indexjs/mymoduleserver.js
+++ zzSandbox/simplify_indexjs/mymoduleserver.js
@@ -2,14 +2,17 @@
 // https://www.w3schools.com/nodejs/nodejs_filesystem.asp
 var http = require('http');
 var dt = require('./myfirstmodule');
+//var dt = eval('./myfirstmodule');
 var fs = require('fs');
-
+//console.log('dt: ', dt);
 
 http.createServer(function(req,res) {
 	fs.readFile('demofile1.html', function(err, data) {
 		res.writeHead(200, {'Content-Type': 'text/html'});
-		res.write(data);
+		//res.write(data);
+		res.write(eval('data'));
 		res.write('The date and time are currently: ' + dt.myDateTime());
+		//res.write(eval(dt));
 		res.end();
 	});
 }).listen(8080);
