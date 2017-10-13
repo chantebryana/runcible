3e86dfe61a30033f87a91df7f47f7077f467bf7c
chantebryana <chantebryana@gmail.com>
2017-10-13 17:00:10 -0500

    clean up, add comments to synchronous fs

--- zzSandbox/simplify_indexjs/mymoduleserver.js
+++ zzSandbox/simplify_indexjs/mymoduleserver.js
@@ -4,15 +4,14 @@
 var http = require('http');
 var dt = require('./myfirstmodule');
 var fs = require('fs');
+// synchronous fs: 
 var data = fs.readFileSync('demofile1.html');
 
 http.createServer(function(req,res) {
-	//fs.readFile('demofile1.html', function(err, data) {
-		res.writeHead(200, {'Content-Type': 'text/html'});
-		res.write(eval('data'));
-		res.write('The date and time are currently: ' + dt.myDateTime());
-		res.end();
-	//});
+	res.writeHead(200, {'Content-Type': 'text/html'});
+	res.write(eval('data'));
+	res.write('The date and time are currently: ' + dt.myDateTime());
+	res.end();
 }).listen(8080);
 
 
