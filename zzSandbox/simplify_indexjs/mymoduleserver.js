de7bbed624f4bd6dec7087d8fcf7bf959f39ab48
chantebryana <chantebryana@gmail.com>
2017-10-17 10:55:41 -0500

    try to extract only .html files from directory and then push that to later code: can't make it work

--- zzSandbox/simplify_indexjs/mymoduleserver.js
+++ zzSandbox/simplify_indexjs/mymoduleserver.js
@@ -4,8 +4,16 @@
 var http = require('http');
 var dt = require('./myfirstmodule');
 var fs = require('fs');
-// synchronous fs: 
+// synchronous fs read file: 
 var data = fs.readFileSync('demofile1.html');
+
+// synchronous fs read directory (using tools discovered / played with in zzSandbox/simplify_indexjs/main.js): 
+// CE: it would be nice to only extract the .html files from the directory, and then push them into an array variable (maybe like data[0] and data[1] and so on), but I can't find the help text to make that work.
+var dir = fs.readdirSync('/home/ruby/Projects/runcible/zzSandbox/simplify_indexjs');
+console.log('synchronously read directory: ');
+for (var i = 0; i < dir.length; i++) {
+	console.log(dir[i], ' ');
+};
 
 http.createServer(function(req,res) {
 	res.writeHead(200, {'Content-Type': 'text/html'});
