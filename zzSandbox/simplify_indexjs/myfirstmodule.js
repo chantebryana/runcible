01566d7273214b2a537c4ac5841775c277c94fbe
chantebryana <chantebryana@gmail.com>
2017-10-18 10:52:29 -0500

    changes? commiting and syncing.

--- zzSandbox/simplify_indexjs/myfirstmodule.js
+++ zzSandbox/simplify_indexjs/myfirstmodule.js
@@ -1,9 +1,10 @@
 // https://www.w3schools.com/nodejs/nodejs_modules.asp
 // using 'exports' keyword to make properties and methods available outside module file:
-
+/*
 // CE: removing exports to try to use require the way Jim mentioned yesterday:
 //exports.myDateTime = function() {
 myDateTime = function() {
 	return Date();
 }
-
+*/
+myDateTime = Date();
