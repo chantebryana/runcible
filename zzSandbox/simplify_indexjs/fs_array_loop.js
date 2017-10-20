7ad6fe83af0794402d934bb91b7523cd517e81c0
chantebryana <chantebryana@gmail.com>
2017-10-20 18:07:22 -0500

    .slice() is better than .includes() or any of the other methods I had tried up to this point

--- zzSandbox/simplify_indexjs/fs_array_loop.js
+++ zzSandbox/simplify_indexjs/fs_array_loop.js
@@ -1,9 +1,8 @@
 dir = ['a.js', 'b.js', 'c.html', 'd.js', 'e.jpg'];
 
 for (var i = 0; i < dir.length; i++) {
-	// if last three characters of string is '.js', then console.log...
-	// .includes() is imperfect (it searches the whole string instead of just the last 3 characters), but it's a step in the right direction: 
-	if (dir[i].includes('.js')) {
+	// if last three characters of string is '.js', then console.log:
+	if (dir[i].slice(-3) == '.js') {
 		console.log(dir[i]);
 	}
 };
@@ -14,5 +13,6 @@ for (var i = 0; i < dir.length; i++) {
 //console.log(dir[0].charAt(dir[0].length -3));
 //subst = dir[0].substring(3, -1);
 //subst = dir[0].substr(-1, 4);
-subst = dir[0].includes('.js');
+//subst = dir[0].includes('.js');
+subst = dir[0].slice(-3);
 console.log(subst)
