ea87c4b4af38b18a83188094cb4edd147f35e9b5
chantebryana <chantebryana@gmail.com>
2017-10-20 17:39:27 -0500

    got mymoduleserver.js to work, thanks to the lessons I learned from Jim!

--- zzSandbox/simplify_indexjs/mymoduleserver.js
+++ zzSandbox/simplify_indexjs/mymoduleserver.js
@@ -5,7 +5,8 @@ var http = require('http');
 
 // CE: removing var to play with global variables like Jim described
 //var dt = require('./myfirstmodule');
-dt = require('./myfirstmodule');
+//dt = require('./myfirstmodule');
+require('./myfirstmodule');
 
 var fs = require('fs');
 // synchronous fs read file: 
@@ -13,7 +14,7 @@ var data = fs.readFileSync('demofile1.html');
 
 // synchronous fs read directory (using tools discovered / played with in zzSandbox/simplify_indexjs/main.js): 
 // CE: it would be nice to only extract the .html files from the directory, and then push them into an array variable (maybe like data[0] and data[1] and so on), but I can't find the help text to make that work.
-var dir = fs.readdirSync('/home/ruby/Projects/runcible/zzSandbox/simplify_indexjs', '*.html');
+var dir = fs.readdirSync('/home/ruby/Projects/runcible/zzSandbox/simplify_indexjs');
 console.log('synchronously read directory: ');
 for (var i = 0; i < dir.length; i++) {
 	console.log(dir[i], ' ');
@@ -23,7 +24,8 @@ http.createServer(function(req,res) {
 	res.writeHead(200, {'Content-Type': 'text/html'});
 	res.write(eval('data'));
 	// res.write('The date and time are currently: ' + dt.myDateTime());
-	res.write(dt);
+	//res.write(dt);
+	res.write(myDateTime);
 	res.end();
 }).listen(8080);
 
