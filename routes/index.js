330cbaac750ff12e37be52c1832a554143f3b37c
chantebryana <chantebryana@gmail.com>
2017-10-20 15:26:44 -0500

    more require progress

--- routes/index.js
+++ routes/index.js
@@ -33,6 +33,7 @@ require('/home/ruby/Projects/runcible/routes/helper_func/run_smart');
 require('/home/ruby/Projects/runcible/routes/helper_func/cookie_pg_load_helpers');
 require('/home/ruby/Projects/runcible/routes/helper_func/get_pg_load');
 require('/home/ruby/Projects/runcible/routes/helper_func/post_pg_load');
+require('/home/ruby/Projects/runcible/routes/helper_func/id_date_range_helpers');
 
 
 
@@ -90,42 +91,6 @@ router.get_pg_load('/form_update', function(req, res, pg_load) {
 	});
 });
 
-// query db for a list of id's from 'cycles' table, ordered by date; pass the array via callback:
-function get_cycle_id_by_date(callback) {
-	db.all('SELECT id FROM cycles ORDER BY begin_date DESC', function(err, rows_from_db) {
-		var cycle_id_array = [];
-		var cycles_length = rows_from_db.length;
-		for (i = 0; i < cycles_length; i++) {
-			cycle_id_array[i] = rows_from_db[i].id;
-		};
-		callback(cycle_id_array);
-	});
-};
-
-// get array of cycle id's via get_cycle_id_by_date(), then save the first and last in their own variables and pass vars via callback:
-function get_first_and_last_cycle_id(callback) {
-	get_cycle_id_by_date(function(cycle_id_array){
-		var first_cycle_id = cycle_id_array[cycle_id_array.length-1];
-		var last_cycle_id = cycle_id_array[0];
-//		console.log('first_cycle_id: ' + first_cycle_id + ', last_cycle_id: ' + last_cycle_id + '\n');
-		callback(first_cycle_id, last_cycle_id);
-	});
-};
-
-// get array of cycle id's via get_cycle_id_by_date(), then save the next and previous (relative to the current cycle id) in their own variables and pass vars via callback:
-function find_next_previous_cycle(current_cycle_id, callback) {
-	get_cycle_id_by_date(function (cycle_id_array){
-		var current_cycle_id_index = 0;
-		for (i = 0; i < cycle_id_array.length; i++) {
-			if (cycle_id_array[i] == current_cycle_id) {
-				current_cycle_id_index = i;
-			};
-		};
-		var previous_cycle_id = cycle_id_array[current_cycle_id_index + 1];
-		var next_cycle_id = cycle_id_array[current_cycle_id_index - 1];
-		callback(previous_cycle_id, next_cycle_id);
-	});
-};
 
 
 
