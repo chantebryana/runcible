57fa1ab5a7e44be5f9d2f135464c03c77ae9db4e
chantebryana <chantebryana@gmail.com>
2017-10-26 20:13:06 -0500

    next steps towards setting up login live. connect login form page to form post page, and then cycle back to the beginning, but this time w/ a query string that marks whether the user is authenticated or not.

--- routes/URL_handlers/005_login.js
+++ routes/URL_handlers/005_login.js
@@ -2,3 +2,9 @@ router.get_pg_load('/login', function(req, res, pg_load) {
 	console.log('Total Page Loads After Loading Login Page: ', pg_load); // not needed for future workflow, but maintaining it for now just because
 	res.render('pages/login.ejs', {title: 'Login'});
 });
+
+/*
+			if (req.query.cycle) {
+				which_cycle_id = current_cycle_id;
+			};
+*/
---
+++ routes/URL_handlers/006_login_post.js
@@ -0,0 +1,16 @@
+router.post_pg_load('/loginpost', function(req, res, pg_load) {
+	console.log('Total Page Loads After Posting Login Form Page: ', pg_load);
+	// query against user_acct table
+	// check if true or false
+	// pass forward true or false results to dummy page for now so I don't have to manipulate home page.
+	db.run_smart("SELECT * FROM user_acct WHERE username = \"" + req.body["username"] + "\" AND password = \"" + req.body["password"] + "\"", function (err, rows) {
+		var user_auth;
+		if (rows.length == 0) {
+			user_auth = false;
+		} else {
+			user_auth = true;
+		};
+		res.redirect('/login?user_auth=' + user_auth);
+		// i could pass data forward w/ res.redirect and a query string that passes user_auth variable.
+	});
+});
