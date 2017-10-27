63f380c4897f3be9a0b390ba0e278ef1b75983a9
chantebryana <chantebryana@gmail.com>
2017-10-26 20:21:22 -0500

    render query string onto web page

--- routes/URL_handlers/005_login.js
+++ routes/URL_handlers/005_login.js
@@ -1,6 +1,13 @@
 router.get_pg_load('/login', function(req, res, pg_load) {
 	console.log('Total Page Loads After Loading Login Page: ', pg_load); // not needed for future workflow, but maintaining it for now just because
-	res.render('pages/login.ejs', {title: 'Login'});
+	var is_logged_in = "";
+	if (req.query.user_auth) {
+		is_logged_in = req.query.user_auth;
+	}
+	res.render('pages/login.ejs', {
+		title: 'Login',
+		is_logged_in_to_renderer: is_logged_in
+	});
 });
 
 /*
