2e003ee997efa462e57eb2d140f68258f819fe8e
chantebryana <chantebryana@gmail.com>
2017-11-01 09:42:50 -0500

    errors with 'key' query string. see code for comments, error reports

--- routes/URL_handlers/006_login_post.js
+++ routes/URL_handlers/006_login_post.js
@@ -13,8 +13,36 @@ router.post('/loginpost', function(req, res) {
 		};
 		// CE: this req.query.key outputs the following query string after being redirected back to the login page: 
 		// http://localhost:3000/login?key=undefined&user_auth=true
-		res.redirect('/login?key=' + req.query.key + '&user_auth=' + user_auth);
+		//res.redirect('/login?key=' + req.query.key + '&user_auth=' + user_auth);
+		res.redirect('/login?key=' + key_to_renderer + '&user_auth=' + user_auth);
 		//res.redirect('logged_in?user_auth=' + user_auth);
 		// i could pass data forward w/ res.redirect and a query string that passes user_auth variable.
 	});
 });
+
+/*
+trying to fix 'key' value query string funkiness (after formpost 'key' is getting redefined from 'null' to 'undefined'). The above change (line 17) gave me this error (server crashed at localhost:3000/loginpost):
+
+ruby@rubyVM:~/Projects/runcible$ node routes/index.js 
+index.js listening on port 3000!
+Cookies from browser:  {}
+cookie_var:  {}
+/home/ruby/Projects/runcible/node_modules/sqlite3/lib/trace.js:27
+                    throw err;
+                    ^
+
+ReferenceError: key_to_renderer is not defined
+    at /home/ruby/Projects/runcible/routes/URL_handlers/006_login_post.js:17:32
+    at Statement.<anonymous> (/home/ruby/Projects/runcible/routes/helper_func/010_run_smart.js:7:6)
+--> in Database#all('SELECT * FROM user_acct WHERE username = "named_user" AND password = "pass0word!"', [Function])
+    at Database.run_smart (/home/ruby/Projects/runcible/routes/helper_func/010_run_smart.js:3:8)
+    at /home/ruby/Projects/runcible/routes/URL_handlers/006_login_post.js:7:5
+    at Layer.handle [as handle_request] (/home/ruby/Projects/runcible/node_modules/express/lib/router/layer.js:95:5)
+    at next (/home/ruby/Projects/runcible/node_modules/express/lib/router/route.js:137:13)
+    at Route.dispatch (/home/ruby/Projects/runcible/node_modules/express/lib/router/route.js:112:3)
+    at Layer.handle [as handle_request] (/home/ruby/Projects/runcible/node_modules/express/lib/router/layer.js:95:5)
+    at /home/ruby/Projects/runcible/node_modules/express/lib/router/index.js:281:22
+    at Function.process_params (/home/ruby/Projects/runcible/node_modules/express/lib/router/index.js:335:12)
+    at next (/home/ruby/Projects/runcible/node_modules/express/lib/router/index.js:275:10)
+
+*/
