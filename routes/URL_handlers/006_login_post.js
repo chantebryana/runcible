96bb91f6a7e40b253ef6f6f06047d445e60de102
chantebryana <chantebryana@gmail.com>
2017-11-03 13:18:11 -0500

    cleaning up

--- routes/URL_handlers/006_login_post.js
+++ routes/URL_handlers/006_login_post.js
@@ -81,32 +81,4 @@ router.post('/loginpost', function(req, res) {
 	});
 });
 
-/*
-	workflow followed: 
-		- clear cookie
-		- type home page into address bar
-		- get auto-redirected to login page
-		- try to login with an invalid login
-		- get auto-redirected back to login
 
-	terminal printout: 
-ruby@rubyVM:~/Projects/runcible$ node routes/index.js 
-010_run_smart.js
-020_user_auth_funcs.js
-050_id_date_range_helpers.js
-060_x-y_axis_helpers.js
-index.js listening on port 3000!
-Cookies from browser:  {}
-cookie_var:  {}
-2a
-Cookies from browser:  { cookie_key: '6LooWk' }
-cookie_var:  { cookie_key: '6LooWk' }
-Cookies from browser:  { is_auth: 'true' }
-cookie_var:  { is_auth: 'true' }
-1a
-
-Question: why is 'is_auth_ set to true? How could that even happen?? 
-I figured it out: I had set it to 'true' in a previous login. I then cleared the cookie (as itemized by the first step), BUT the clear cookie brains didn't include clearing the 'is_auth' cookie, just the 'cookie_key' cookie. I fixed 'clearcookie' file and now it all works as expected! Yay!
-
-Also, all of the workflows seem to work within the cookie-based lookup. 
-*/
