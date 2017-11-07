dd55b9afae0555ed90173d61741e4f65bc1439e6
chantebryana <chantebryana@gmail.com>
2017-11-07 13:31:05 -0600

    w/ Jim re session_data object and helper functions for login_post.js

--- routes/helper_func/040_login_post_helper.js
+++ routes/helper_func/040_login_post_helper.js
@@ -2,10 +2,10 @@
 
 // if a user logs in correctly, their unauthorized session becomes authorized (by setting user_auth to true): 
 authorize_db_session_data = function authorize_db_session_data(cookie_key, session_data, callback) {
-	session_data.user_auth = "true";
+	session_data.user_auth = "true"; // JE: pointer to user_auth variable within session_data object is manipulated -- a new copy of session_data isn't created over top the old one. long story short, I'll have access to the updated session_data elswhere in my code b/c of pointers -- I don't need to manually pass forward what I've changed here. This also means I don't have to include any arguments within the callback on line 8.
 	session_string = JSON.stringify(session_data);
 	db.run_smart("UPDATE cookie_key_json SET session_data = ? WHERE cookie_key = ?", session_string, cookie_key, function(err, rows) {
-		callback(session_data);
+		callback();
 	});
 };
 
