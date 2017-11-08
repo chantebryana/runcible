57443dbabf6707647fd7888a13c85cdd5fb90ac9
chantebryana <chantebryana@gmail.com>
2017-11-08 10:39:11 -0600

    remove redundant files (after last commit's reorganization)

--- routes/helper_func/zzretired/032_post_with_session.js
+++
@@ -1,8 +0,0 @@
-// `post` wrapper that identifies or creates session_data:
-router.post_with_session = function post_with_session(url, url_handler_callback) {
-	this.post(url, function (req, res) {
-		find_or_start_session(req, res, function (session_data) {
-			url_handler_callback(req, res, session_data);
-		});
-	});
-};
