308eaad2f462093fdc28f5121c29bb7dc134c80d
chantebryana <chantebryana@gmail.com>
2017-11-14 11:10:29 -0600

    remove `render` wrapper attempt for `save_session` (old code)

--- routes/helper_func/041_pg_load_helpers.js
+++ routes/helper_func/041_pg_load_helpers.js
@@ -14,42 +14,7 @@ increment_pg_load = function increment_pg_load(session_data, pg_session_callback
 	}
 };
 
-/*
-// CE: (failed) attempt to wrap if/else res.render branches at the end of home_pg.js into its own black box. got error messages and doesn't work. Jim provided some feedback (in notes of home_pg.js) to give me stuff to latch onto while I move forward.
-res.render_pg_load = function render_pg_load(view, locals, req, pg_session, session_data) {
-	if (pg_session != session_data) {
-		var browser_key = req.cookies;
-		var session_string = JSON.stringify(pg_session);
-		db.run_smart("UPDATE cookie_key_json SET session_data = ? WHERE cookie_key = ?", session_string, browser_key.cookie_key, function (err, rows) {
-			this.render(view, locals);
-		});
-	} else {
-		this.render(view, locals);
-	}
-}
-*/
-/*
-final_render_step = function() {
-	res.render('pages', {
-		title: 'Home', 
-		// rough hack: attempt to prevent homepage from breaking when there's a new cycle that has no child entries: if the 0-th element of rows_from_db exists, link rows_to_renderer to rows_from_db, otherwise, link rows_to_renderer to empty array object:
-		rows_to_renderer: rows_from_db[0] ? rows_from_db : [{}], 
-		// render beginning and end dates of currently displayed cycle to index.ejs:
-		begin_date_to_renderer: begin_date,
-		end_date_to_renderer: end_date,
-		y_temp_f_to_renderer: y_temp_f, 
-		x_label_values_to_renderer: x_label_values,
-		//pg_load_to_renderer: pg_load,
-		cycle_id_to_renderer: {
-			prev: previous_cycle_id, 
-			curr: current_cycle_id, 
-			next: next_cycle_id, 
-			first: first_cycle_id, 
-			last: last_cycle_id
-		}
-	});
-};
-*/
+
 
 save_session_data = function save_session_data(session_data, pg_session, res, browser_key, save_callback) {
 	var browser_key = browser_cookie;
