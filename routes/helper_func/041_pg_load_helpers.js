6c634f5a49bb31eec6715d228da54d20d9e6af94
chantebryana <chantebryana@gmail.com>
2017-11-15 10:15:00 -0600

    a few notes as I play with putting wrapper around `res.render`

--- routes/helper_func/041_pg_load_helpers.js
+++ routes/helper_func/041_pg_load_helpers.js
@@ -7,3 +7,27 @@ save_session_data = function save_session_data(session_data, res, browser_key, s
 		save_callback();
 		});
 };
+
+//
+//
+//
+//
+//CE: does this wrapper work just like res.render()?
+res.render_with_session = function render_with_session(view, locals, render_callback) {
+	this.render(view, locals, function() {
+		render_callback();
+	});
+};
+
+//
+//
+//
+//
+//CE: attempting to include `save_session_data` in this wrapper: does it work?
+res.render_with_session = function render_with_session(view, locals, render_callback) {
+	this.render(view, locals, function() {
+		save_session_data(session_data, res, browser_key, function() {
+			render_callback();
+		});
+	});
+};
