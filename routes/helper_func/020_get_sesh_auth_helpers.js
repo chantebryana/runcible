4813fbc197c29a3ed9b3c10c9a29ef94d709b87b
chantebryana <chantebryana@gmail.com>
2017-11-20 11:09:04 -0600

    make cookie generated in `make_id` 4 times longer than before (more security)

--- routes/helper_func/020_get_sesh_auth_helpers.js
+++ routes/helper_func/020_get_sesh_auth_helpers.js
@@ -5,7 +5,7 @@ make_id = function make_id() {
 	//console.log("make_id() is running!");
 	var text = "";
 	var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-	for (var i = 0; i < 6; i++) {
+	for (var i = 0; i < 24; i++) {
 		text += possible.charAt(Math.floor(Math.random() * possible.length));
 	};
 	return text;
