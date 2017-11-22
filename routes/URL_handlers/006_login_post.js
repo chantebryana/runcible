2d98d1fdc6c537e234c78668405f7aaac763a1c1
chantebryana <chantebryana@gmail.com>
2017-11-22 10:51:07 -0600

    first pass on SHA-256 hash encryption: works

--- routes/URL_handlers/006_login_post.js
+++ routes/URL_handlers/006_login_post.js
@@ -1,6 +1,19 @@
 // moved helper functions to routes/helper_func/020_user_auth_func.js
 
 router.post_with_session('/loginpost', function(req, res, session_data) {
+	//CE PUT ENCRYPTION HERE
+	// source: https://nodejs.org/api/crypto.html#crypto_class_hash
+	crypto = require("crypto");
+	hash = crypto.createHash("sha256");
+	hash.on("readable", function() {
+		data = hash.read();
+		if (data) {
+			console.log(data.toString("hex"));
+			// returns fb0f65bad4dc6f57274d18f5296dd44c5709f29dfed319d8b42ffd760360aa00
+		}
+	});
+	hash.write("mysecretpassword\n");
+	hash.end();
 	db.run_smart ("SELECT * FROM user_acct WHERE username = ? AND password = ?", req.body["username"], req.body["password"], function(err, rows) {
 		if (rows.length == 0) { // if login failed
 			return res.redirect("/login?user_auth=false");
@@ -13,5 +26,4 @@ router.post_with_session('/loginpost', function(req, res, session_data) {
 		}
 	});
 });
-
 
