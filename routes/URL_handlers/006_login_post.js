2285d3319656154baaa40b8bd37ceb8d3b986eba
chantebryana <chantebryana@gmail.com>
2017-11-22 10:45:18 -0600

    minor clean-ups

--- routes/URL_handlers/006_login_post.js
+++ routes/URL_handlers/006_login_post.js
@@ -1,9 +1,7 @@
 // moved helper functions to routes/helper_func/020_user_auth_func.js
 
 router.post_with_session('/loginpost', function(req, res, session_data) {
-	//cookie_var = req.cookies; // {cookie_key:"abc123", is_auth:"false"} or {} 
-	//db.run_smart ("SELECT * FROM user_acct WHERE username = \"" + req.body["username"] + "\" AND password = \"" + req.body["password"] + "\"", function(err, rows_ua) {
-db.run_smart ("SELECT * FROM user_acct WHERE username = ? AND password = ?", req.body["username"], req.body["password"], function(err, rows) {
+	db.run_smart ("SELECT * FROM user_acct WHERE username = ? AND password = ?", req.body["username"], req.body["password"], function(err, rows) {
 		if (rows.length == 0) { // if login failed
 			return res.redirect("/login?user_auth=false");
 		} else { // if login successful
